3e4a6cd03957771715f0c24e7f989dcbbc40d644
Аман Азат уулу <a.aman@invescore.kg>
2023-06-09 16:30:57 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,95 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        byte num = 2; // маленькие числа
+//        int i = 234567; // больше
+//        long l = 452232225;
+//        float num_2 = 324.234f; // c плавающей точкой, в конце f ставить обязательно
+//        double num_3;   // тот же флоат, только диапозон больше в два раза
+//        num_3 = 2354.234f;
+//
+//        char sym = 'A';
+//
+//        boolean is_happy = true; // true is 1, false if 0. не является строкой, лишь логическая переменная
+//
+//        String str = "Hello World!";
+
+//        System.out.println("variable = " + str); // println - это перенос на нижюю строку
+
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+//        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+//            System.out.println("i = " + i);
+
+        // ввод пользователя числа
+//        Scanner num4 = new Scanner(System.in);
+//        int i2;
+//        i2 = num4.nextInt();
+//        System.out.print("My variable is: " + i2);
+
+//       ввод стринга
+//        Scanner str2 = new Scanner(System.in);
+//        String symbols;
+//        symbols = str2.nextLine(); // <------- for string для каждого типа переменной , есть свой вывод next
+//        System.out.print("My variable is " + symbols);
+
+        // Математические операции
+//        float num_1 = 23.67f, num_2 = 43.4f, res; // создание нескольких переменных
+//        res = num_1 + num_2;
+//        res++; // res = res + 1
+//        System.out.print(res);
+
+        // Условные операторы
+//        int i = 234, j = 45;
+//        if (i != 234 || j == 45 ) { // && umpersanty. || == or
+//            System.out.print("i is not 234");
+//        } else if (i > 100) {
+//            System.out.print("i is bigger than 100");
+//        } else {
+//            System.out.print("i is unkown");
+//        }
+
+        // Условный оператор Switch key
+//        int num = 2;
+//        switch (num) {
+//            case 12:
+//                System.out.print("Num is 12");
+//                break;
+//            case 545:
+//                System.out.print("Num is 545");
+//                break;
+//            case 23:
+//                System.out.print("Num is 23");
+//                if (num == 23)
+//                    System.out.println("All is ok!");
+//                break;
+//            default:
+//                System.out.print("Num is unkown");
+//        }
+
+        // цикл For
+//        for (int i = 100; i >=10; i-= 10)
+//            System.out.println("Element i is: " + i);
+
+        // Цикл While
+//        int i = 0;
+//        while (i <= 15) {
+//            System.out.println("Element i is: " + i);
+//            i++;
+//        }
+
+
+
+        }
+    }
+//}
